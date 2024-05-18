4601e1796e16133bffebe5756b3e97a035b9788b
Sansar Tiwari <89459552+Sansar01@users.noreply.github.com>
2024-05-18 11:19:35 +0530

    Mega_Blog_Project

---
+++ components/Index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Container from './container/container'
+import LogoutBtn from './Header/LogoutBtn'
+import Logo from './Logo'
+import Input from './Input'
+import Button from './Button'
+import Select from './Select'
+import SignUp from './SignUp'
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './PostForm/PostForm'
+import AuthLayout from '../components/AuthLayout'
+import RTE from '../components/RTE'
+
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    Input,
+    Button,
+    Select,
+    SignUp,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    RTE
+}
